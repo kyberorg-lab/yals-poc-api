0c71717115928f67bb4a614cbdbec5868a868af0
Alex Muravya <kyberorg@yadev.eu>
2020-09-29 16:25:28 +0300

    Init commit

---
+++ src/main/java/eu/kyberorg/yals/api/YalsApiApplication.java
@@ -0,0 +1,13 @@
+package eu.kyberorg.yals.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YalsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YalsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/kyberorg/yals/api/YalsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.kyberorg.yals.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YalsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
