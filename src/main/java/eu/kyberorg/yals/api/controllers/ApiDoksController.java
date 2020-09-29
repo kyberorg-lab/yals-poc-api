7d6e92f82165a0631714ddc87e829ca0bcbb1c20
Alex Muravya <kyberorg@yadev.eu>
2020-09-29 16:33:24 +0300

    API Doks

---
+++ src/main/java/eu/kyberorg/yals/api/controllers/ApiDoksController.java
@@ -0,0 +1,21 @@
+package eu.kyberorg.yals.api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/")
+public class ApiDoksController {
+
+	@GetMapping({"/", "/api.html"})
+	public RedirectView redirectWithUsingRedirectView(
+		RedirectAttributes attributes) {
+
+		attributes.addFlashAttribute("flashAttribute", "redirectWithRedirectView");
+		attributes.addAttribute("attribute", "redirectWithRedirectView");
+		return new RedirectView("/swagger-ui/index.html?url=/api-docs");
+	}
+}
