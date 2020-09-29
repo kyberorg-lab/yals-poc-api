cdb8e741394395c694a1252dcfd053b2e4b9c9b1
Alex Muravya <kyberorg@yadev.eu>
2020-09-29 17:00:13 +0300

    Stub Endpoint

---
+++ src/main/java/eu/kyberorg/yals/api/StubMessage.java
@@ -0,0 +1,8 @@
+package eu.kyberorg.yals.api;
+
+import lombok.Data;
+
+public @Data
+class StubMessage {
+	final String message;
+}
---
+++ src/main/java/eu/kyberorg/yals/api/controllers/StubRestController.java
@@ -0,0 +1,16 @@
+package eu.kyberorg.yals.api.controllers;
+
+import eu.kyberorg.yals.api.StubMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("stub")
+public class StubRestController {
+
+	@GetMapping("/")
+	public StubMessage stub() {
+		return new StubMessage("hello hello");
+	}
+}
