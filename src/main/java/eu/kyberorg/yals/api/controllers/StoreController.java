3a9da0600c459b7ef01ae7c9243672f893eec8ff
Alex Muravya <kyberorg@yadev.eu>
2020-10-02 15:26:19 +0300

    Store Rest Endpoint

--- src/main/java/eu/kyberorg/yals/api/controllers/StoreController.java
+++ src/main/java/eu/kyberorg/yals/api/controllers/StoreController.java
@@ -1,22 +1,20 @@
 package eu.kyberorg.yals.api.controllers;
 
 import eu.kyberorg.yals.api.core.IdentGenerator;
-import eu.kyberorg.yals.api.jsons.ErrorJson;
-import eu.kyberorg.yals.api.jsons.YalsJson;
 import eu.kyberorg.yals.api.jsons.store.StoreInput;
 import eu.kyberorg.yals.api.jsons.store.StoreOutput;
 import eu.kyberorg.yals.api.models.Link;
 import eu.kyberorg.yals.api.services.LinkService;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
-@RestController("/store")
+@RestController
 public class StoreController {
 
 	private final LinkService linkService;
@@ -25,22 +23,17 @@ public class StoreController {
 		this.linkService = linkService;
 	}
 
-	@RequestMapping(method = RequestMethod.POST, value = "")
-	public ResponseEntity<YalsJson> store(final @RequestBody StoreInput storeInput) {
+	@PostMapping(value = "/store",
+		consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<StoreOutput> store(final @RequestBody StoreInput storeInput) throws Exception {
 		String linkToStore = storeInput.getLink();
-
-		try {
-			Link storedLink = linkService.storeNew(IdentGenerator.generateNewIdent(), linkToStore);
-			URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{ident}")
-				.buildAndExpand(storedLink.getIdent())
-				.toUri();
-			StoreOutput output = StoreOutput.builder().ident(storedLink.getIdent()).build();
-			return ResponseEntity.created(uri).body(output);
-		} catch (Exception e) {
-			ErrorJson errorJson = ErrorJson.builder().errorMessage(e.getMessage()).build();
-			return ResponseEntity.status(500).body(errorJson);
-		}
+		Link storedLink = linkService.storeNew(IdentGenerator.generateNewIdent(), linkToStore);
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath()
+			.path("/get/{ident}")
+			.buildAndExpand(storedLink.getIdent())
+			.toUri();
+		StoreOutput output = StoreOutput.builder().ident(storedLink.getIdent()).build();
+		return ResponseEntity.created(uri).body(output);
 	}
 
 }
